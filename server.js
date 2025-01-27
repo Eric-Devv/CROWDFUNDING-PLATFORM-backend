c178a873fcf26cc9e91d321877bcf0847c0aaafb
BRIGHTGAINSCOMPANY <162323582+BRIGHTGAINSCOMPANY@users.noreply.github.com>
2025-01-27 23:02:29 +0300

    made server and database configurations

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 require("dotenv").config();
 
 const express = require("express");
+const { Mongoose } = require("mongoose");
+const routes = require('./routes/routes')
+
+
 
 const app = express();
 
 
 // middlewares
 app.use(express.json());
+
+
+// DATABASE CONFIGURATIONS
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => {
+        console.log("Connected to mongoDB")
+    })
+
+// Universal Server Routing
+app.use('/api/investors/', routes)
+
+
+// Setting up server action on PORT values
+app.listen(process.env.PORT, () => {
+    console.log(`connected to localhost:${PORT} & now listening for request`)
+})
 
 
